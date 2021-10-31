4c5a97706804525b369eb31969dcf6ee73e1018d
François Agneray <francois.agneray@lam.fr>
2021-10-31 21:40:53 +0100

    Ajout d'un formulaire login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title: string = 'client';
+    email: string = '';
+    password: string = '';
+    token: string = '';
+
+    constructor(private httpClient: HttpClient) { }
+
+    login() {
+        this.httpClient.post<{userId: number, token: string}>('http://localhost:3000/users/login', { email: this.email, password: this.password}).subscribe(data => {
+            this.token = data.token;
+        });
+    }
 }
